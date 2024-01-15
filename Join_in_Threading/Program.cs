4a59346fcb63a97fc919d702678140ba4d2fde19
Pankajmore2001 <84670901+Pankajmore2001@users.noreply.github.com>
2024-01-15 19:16:28 +0530

    Add files via upload

---
+++ Join_in_Threading/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+namespace Join_in_Threading{
+    public class Program{
+        static void Test1()
+        {
+            Console.WriteLine("Thread1 is started.");
+            for(int i =1;i<=25;i++)
+            {
+                Console.WriteLine("Test1: "+i);
+            }
+            Console.WriteLine("Thread1 is exiting.");
+        }
+        static void Test2()
+        {
+            Console.WriteLine("Thread2 is started.");
+            for(int i =1;i<=25;i++)
+            {
+                Console.WriteLine("Test2: "+i);
+            }
+            Console.WriteLine("Thread2 is exiting.");
+        }
+        static void Test3()
+        {
+            Console.WriteLine("Thread3 is started.");
+            for(int i =1;i<=25;i++)
+            {
+                Console.WriteLine("Test3: "+i);
+            }
+            Console.WriteLine("Thread3 is exiting.");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Main Thread Started.");
+            Thread T1 = new Thread(Test1);
+            Thread T2 = new Thread(Test2);
+            Thread T3 = new Thread(Test3);
+            T1.Start();T2.Start();T3.Start();
+            T1.Join();T2.Join();T3.Join();
+            Console.WriteLine("Main Thread is Exiting.");
+        }
+    }
+}
---
+++ Lambda_Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Lambda_Practice{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        List<int> numbers = new List<int>(){9,10, 12, 15, 16, 18};
+        Console.WriteLine("The List:");
+        foreach(var value in numbers)
+        {
+            Console.Write(value+" ");
+        }
+        Console.WriteLine();
+        var Square = numbers.Select(x=>x*x);
+        Console.WriteLine("The Square List:");
+        foreach(var value in Square)
+        {
+            Console.Write(value+" ");
+        }
+        List<int> Divby3 = numbers.FindAll(x=>(x%3)==0);
+        Console.WriteLine();
+        Console.WriteLine("The numbers Divide by 3:");
+        foreach(var v in Divby3)
+        {
+            Console.Write(v+" ");
+        }
+        }
+    }
+}
