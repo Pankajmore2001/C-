3d0c3b3d5b7160ca037fd432585dd094cc796e2d
Pankajmore2001 <84670901+Pankajmore2001@users.noreply.github.com>
2024-01-15 19:13:08 +0530

    Add files via upload

---
+++ Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace Practice
+{
+public class shape
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("This is shape");
+        }
+        
+    }
+public class Circle: shape
+    {
+        public override void Display()
+        {
+            Console.WriteLine("This is Circle");
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+       Circle C = new Circle();
+       C.Display();
+
+        }
+       
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Properties{
+    public class Circle
+    {
+        public double _Radius = 12.34;
+        public double Radius
+        {
+            get{return _Radius;}    //Represent a value returnig method without parameter
+            set                     //Represent a non-value returning method with parameter
+            {
+                if(value>_Radius)
+                _Radius = value;
+            }
+        }
+    }
+    public class TestCircle
+    {
+        static void Main()
+        {
+            Circle c = new Circle();
+            double radius = c.Radius;
+            Console.WriteLine(radius);
+            c.Radius = 12.33;
+            Console.WriteLine(c._Radius);
+        }
+    }
+
+}
