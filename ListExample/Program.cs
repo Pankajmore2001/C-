6ea09a2ab4a7ff4e83d456a43247b408bc5a1d8e
Pankajmore2001 <84670901+Pankajmore2001@users.noreply.github.com>
2024-01-15 19:16:32 +0530

    Add files via upload

---
+++ ListExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListExample{
+    public class Program{
+        static void Main(string [] args)
+        {
+            List<int> li = new List<int>();
+            li.Add(10);
+            li.Add(20);
+            li.Add(30);
+            li.Add(40);
+            li.Add(50);
+
+            for(int i =0; i<li.Count;i++)
+            {
+                Console.Write(li[i]+" ");
+            }
+            Console.WriteLine();
+            li.Insert(3,35);        //Inserting an item at location 3 
+
+            foreach(int v in li)
+            {
+                Console.Write(v+" ");
+            }
+            Console.WriteLine();
+
+            li.Remove(35);          //removing an item 35
+            foreach(int v in li)
+            {
+                Console.Write(v+" ");
+            }
+            Console.WriteLine();
+
+            li.RemoveAt(3);         //removing item at location 3 
+            foreach(int v in li)
+            {
+                Console.Write(v+" ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MulticastDelegate;
+public delegate void RectDelegate(double Width,double Height);
+public class Rectangle
+{
+    public void Area(double Width,double Height)
+    {
+        Console.WriteLine("Area of Rectangle: "+ Width*Height);
+    }
+    public void Perimeter(double Width,double Height)
+    {
+        Console.WriteLine("Perimeter of Rectangle: "+ 2*(Width+Height));
+    }
+    static void Main(String[] args)
+    {
+        Rectangle R = new Rectangle();
+        RectDelegate RD = R.Area;
+       // RD-=R.Perimeter;
+        RD(3,4);
+    }
+}
