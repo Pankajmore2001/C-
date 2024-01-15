1a981283a16c2e48a48277eb3bf71b38ec35cf9f
Pankajmore2001 <84670901+Pankajmore2001@users.noreply.github.com>
2024-01-15 19:11:40 +0530

    Add files via upload

---
+++ Structure/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Structure{
+    struct Mystruct
+    {
+        int i;
+        public Mystruct(int i)
+        {
+            this.i=i;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Display Method inside Structure with Value of i "+ i);
+        }
+    static void Main()
+    {
+        Mystruct M ;
+        M.i=10;
+        M.Display();
+        Mystruct M2 = new Mystruct();   //parameterless constructure
+        M2.Display();
+        Mystruct M3 = new Mystruct(30); //parametirised constructure
+        M3.Display();
+
+    }
+    }
+}
---
+++ ThreadingDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+namespace ThreadingDemo{
+    public class Program
+    {
+        static void test1()
+        {
+            for(int i = 1;i<=100;i++)
+            {
+                Console.WriteLine("Test1: "+ i);
+            }
+        }
+
+        static void test2()
+        {
+            for(int i = 1;i<=100;i++)
+            {
+                if(i == 50)
+                {
+                    Console.WriteLine("Thread is Slpeeping");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Thread is awake");
+                }
+                Console.WriteLine("Test2: "+ i);
+            }
+        }
+        static void test3()
+        {
+            for(int i = 1;i<=100;i++)
+            {
+                Console.WriteLine("Test3: "+ i);
+            }
+        }
+        static void Main()
+        {
+            test1();
+            test2();
+            test3();
+        }
+}
+}
