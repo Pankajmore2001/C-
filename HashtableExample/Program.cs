5f0a879fe86338bcb7466c5cdf7cc34d61b96209
Pankajmore2001 <84670901+Pankajmore2001@users.noreply.github.com>
2024-01-15 19:18:03 +0530

    Add files via upload

---
+++ HashtableExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+namespace HashtableExample{
+    class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("Eid",100);
+            ht.Add("Ename","Pankaj");
+            ht.Add("Job","Software Developer");
+            ht.Add("Salary",25000);
+            ht.Add("Mgrid",1000);
+            ht.Add("Phone","9405537494");
+            ht.Add("Email","morep9876@gmail.com");
+            ht.Add("Dname","IT");
+            ht.Add("Location","Pune");
+            ht.Add("Did",30);
+
+            foreach(object key in ht.Keys)
+            {
+                Console.WriteLine(key+":"+ht[key]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Interface{
+    interface ITestinterface1
+    {
+        void add(int x,int y);
+    }
+    interface ITestinterface2 : ITestinterface1
+    {
+        void sub(int x,int y);
+    }
+    public class TestImplementation:ITestinterface2
+    {
+        public void add(int x,int y)
+        {
+            Console.WriteLine(x+y);
+        }
+        public void sub(int x,int y)
+        {
+            Console.WriteLine(x-y);
+        }
+         static void Main()
+        {
+        TestImplementation T = new TestImplementation();
+        T.add(20,45);
+        T.sub(67,66);
+        Console.ReadLine();
+        }
+        
+    }
+
+}
