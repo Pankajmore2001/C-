b5d0c72566911eb73a3d28f323dd05d88b8a68fb
Pankajmore2001 <84670901+Pankajmore2001@users.noreply.github.com>
2024-01-15 19:18:11 +0530

    Add files via upload

---
+++ GenericList_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GenericList_Example{
+    public class Customer{
+        public int Custid{get; set;}
+        public string Name{get;set;}
+        public string City{get;set;}
+        public int Balance{get;set;}
+    }
+    public class TestClass{
+        static void Main()
+        {
+            List<Customer> Customers = new List<Customer>();
+            Customer c1 = new Customer{Custid = 101, Name = "Pankaj" , City = "Pune", Balance = 25000};
+            Customer c2 = new Customer{Custid = 102, Name = "Suraj" , City = "Nashik", Balance = 29000};
+            Customer c3 = new Customer{Custid = 103, Name = "Gaurav" , City = "Mumbai", Balance = 30000};
+            Customer c4 = new Customer{Custid = 104, Name = "Vidya" , City = "Dhule", Balance = 50000};
+
+            Customers.Add(c1);Customers.Add(c2);
+            Customers.Add(c3);Customers.Add(c4);
+            foreach(Customer obj in Customers)
+            {
+                Console.WriteLine(obj.Custid + "\t"+ obj.Name + "\t"+ obj.City+ "\t"+ obj.Balance);
+            }
+
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Indexers;
+namespace Indexers
+{
+    public class Employee
+    {
+        int Eno;
+        double Salary;
+        string Ename,Job,Dname,Location;
+        public Employee(int Eno,string Ename,string Job, double Salary, string Dname, string Location)
+        {
+            this.Eno = Eno;
+            this.Ename = Ename;
+            this.Job = Job;
+            this.Salary = Salary;
+            this.Dname = Dname;
+            this.Location= Location;
+        }
+
+        //Indexers
+        public object this[int index]
+        {
+            get
+            {
+                if(index == 0)
+                    return Eno;
+                else if(index == 1)
+                    return Ename;
+                else if(index == 2)
+                    return Job;
+                else if(index == 3)
+                    return Salary;
+                else if(index == 4)
+                    return Dname;
+                else if(index == 5)
+                    return Location;
+                return null;
+
+            }
+            set
+            {   
+                if(index == 0)
+                    Eno=(int)value;
+                else if(index == 1)
+                    Ename = (string)value;
+                else if(index == 2)
+                    Job = (string)value;
+                else if(index == 3)
+                    Salary = (double)value;
+                else if(index == 4)
+                    Dname = (string)value;
+                else if(index == 5)
+                    Location = (string)value;
+                
+            }
+        }
+    }
+    public class TestEmployee
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp = new Employee(100,"Pankaj","Manager",25000,"IT","Mumbai");
+
+            //using Indexer to get value
+            Console.WriteLine("Eno: "+Emp[0]);
+            Console.WriteLine("Ename: "+Emp[1]);
+            Console.WriteLine("Job: "+Emp[2]);
+            Console.WriteLine("Salary: "+Emp[3]);
+            Console.WriteLine("Dname: "+Emp[4]);
+            Console.WriteLine("Location: "+Emp[5]);
+
+            //using indexers to modify values
+            Emp[2]="Sr.Manager";
+            Emp[3]=300000.00;
+            Console.WriteLine();
+            Console.WriteLine("Eno: "+Emp[0]);
+            Console.WriteLine("Ename: "+Emp[1]);
+            Console.WriteLine("Job: "+Emp[2]);
+            Console.WriteLine("Salary: "+Emp[3]);
+            Console.WriteLine("Dname: "+Emp[4]);
+            Console.WriteLine("Location: "+Emp[5]);
+
+        }
+    }
+}
